334098ace442e0d0afb9f6e187ac3c2b451193ef
MechanicKabya <152405423+MechanicKabya@users.noreply.github.com>
2026-02-26 15:23:54 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const mqtt = require('mqtt');
+const admin = require('firebase-admin');
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mycuetbus-default-rtdb.firebaseio.com"
+});
+
+const db = admin.database();
+
+const client = mqtt.connect('mqtt://mqtt-dashboard.com:1883');
+
+client.on('connect', () => {
+  console.log('Connected to MQTT');
+  client.subscribe('GPS_Tracking_Data');
+});
+
+client.on('message', (topic, message) => {
+  const data = JSON.parse(message.toString());
+  console.log("Received:", data);
+
+  db.ref("GPS_Tracker").push(data);
+});
